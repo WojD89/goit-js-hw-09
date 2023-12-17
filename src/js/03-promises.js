ccadfea5bc480b3218fe0417c594a38b40acb6fb
WojD89 <wojciech.dyrka@gmail.com>
2023-12-17 17:27:58 +0100

    feat: task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delay = parseInt(form.elements['delay'].value, 10);
+    const step = parseInt(form.elements['step'].value, 10);
+    const amount = parseInt(form.elements['amount'].value, 10);
+
+    for (let i = 0; i < amount; i++) {
+      const currentDelay = delay + i * step;
+
+      createPromise(i + 1, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+  });
+});
