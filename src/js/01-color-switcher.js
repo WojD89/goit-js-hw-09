fdc26ab5bbe466af8cbe660d91b1e76c5495d627
WojD89 <wojciech.dyrka@gmail.com>
2023-12-17 16:38:13 +0100

    feat: task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBackground = document.querySelector('body');
+let intervalSetup;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const backgroundColorStart = function () {
+  intervalSetup = setInterval(() => {
+    bodyBackground.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+};
+
+const backgroundColorStop = function () {
+  clearInterval(intervalSetup);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+};
+
+startBtn.addEventListener('click', backgroundColorStart);
+stopBtn.addEventListener('click', backgroundColorStop);
